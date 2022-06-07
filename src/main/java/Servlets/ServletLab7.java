e12d20f1dd5f6ac5d11b05810e44517cd1a212c1
DiegoZuasnabar <a20190459@pucp.edu.pe>
2022-06-06 20:05:01 -0500

    Avance lab

---
+++ src/main/java/Servlets/ServletLab7.java
@@ -0,0 +1,24 @@
+package Servlets;
+
+import Daos.ActoresDao;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ServletLab7", value = "/ServletLab7")
+public class ServletLab7 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ActoresDao actoresDao = new ActoresDao();
+        request.setAttribute("listaActores", actoresDao.listarActores());
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("pantalla.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
